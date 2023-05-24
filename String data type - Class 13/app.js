21bf126da7ac2401aef67ddd1394b02516e8ad19
devlopersabbir <devlopersabbir@gmail.com>
2023-05-25 01:28:52 +0600

    Class 13 - String Data type

---
+++ String data type - Class 13/app.js
@@ -0,0 +1,10 @@
+console.clear();
+
+const age = 400;
+// const message = `Hi, \"I learned from ST Sabbir\". My age is ${age}`; // ${}
+
+const about = "Hi I'm Sabbir. " + age;
+const about2 = `Hello, viewers. My subscriber is ${age}`;
+// const about3 = 'hello I'm sabbir ';
+
+console.log(about2);
