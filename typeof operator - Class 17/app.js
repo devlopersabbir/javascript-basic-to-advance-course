0f7c1530eb8715e68a6c12908cd7c837b7a26115
devlopersabbir <devlopersabbir@gmail.com>
2023-08-31 05:44:42 +0600

    typeof operator - class 17

---
+++ typeof operator - Class 17/app.js
@@ -0,0 +1,16 @@
+console.log(typeof 20); // number
+// let name = "Sabbir";
+// console.log(typeof name); // string
+
+// typeof()
+// typeof
+
+console.log(typeof "Sabbir"); // string
+console.log(typeof 120); // number
+console.log(typeof 12n); // Bigint
+console.log(typeof true); // boolean
+console.log(typeof undefined); // undefind
+console.log(typeof null); // object
+
+typeof null === null ? console.log(true) : console.log(false);
+// typeof null is "object" then it's return true.
