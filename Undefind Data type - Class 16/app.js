ed27d8c4cd2103df6d588066a5f8dd037b8aa2f1
devlopersabbir <devlopersabbir@gmail.com>
2023-08-31 05:09:43 +0600

    Undefind Data type

---
+++ Undefind Data type - Class 16/app.js
@@ -0,0 +1,17 @@
+let myName;
+
+// sdjakfasf;
+// afjakslfjasf;
+// fdsajfkajsfk;
+// asfdsf;
+
+// myName = "Sabbir";
+
+jodi myName is null and myName is "" and myName is undefined 
+
+{
+    hay myName empty
+}
+{
+    // task perform here...
+}
