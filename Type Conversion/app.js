4f13f705cb69069ecf066c77b0d69dea691f8065
devlopersabbir <devlopersabbir@gmail.com>
2023-09-11 01:18:32 +0600

    JavaScript type Conversion

---
+++ Type Conversion/app.js
@@ -0,0 +1,39 @@
+/**
+ * String To any
+ * Number to any
+ * Boolean to any
+ */
+
+// const firstNumber = prompt("Enter your first number");
+// const lastNumber = prompt("Enter your last number");
+
+const bool = Boolean(0); // null|undefined|""|NaN|0 ---> false
+const bool2 = Boolean("Sabbir"); // "Sabbir"|1|10000|"0"|" " ---> true
+// const num2 = Number(lastNumber);
+
+console.log(bool2);
+
+const str = String(20);
+console.log(str);
+
+const num = Number(false);
+console.log(num);
+
+// console.log()
+// let age = "19";
+// console.log(`Age is: ${typeof age}`); // string
+
+// let ageChanged = Number(age);
+// console.log(`Age is: ${typeof ageChanged}`); // number
+
+// let a = 20;
+// let res = String(a) + 20; // "20"+20 => 2020
+// console.log(typeof res); // string
+
+// let b = true; // true -> 1 | false -> 0
+// let bChan = String(b);
+// console.log(typeof bChan);
+
+// console.log(typeof Number(b));
+
+// let c = null;
