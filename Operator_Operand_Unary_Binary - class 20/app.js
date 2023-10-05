d966baa57a6f4be1e3c461b10bef9b04474f78c1
devlopersabbir <devlopersabbir@gmail.com>
2023-10-05 15:52:40 +0600

    🌟 JS Operator, Operand, Unary, Binary - Class 20

---
+++ Operator_Operand_Unary_Binary - class 20/app.js
@@ -0,0 +1,46 @@
+// JS Operator & Operand
+// (+) -> Addition
+// (-) -> Subtraction
+// (*) -> Multiplication
+// (/) -> Division
+// (**) -> Exponentiation
+// (%) -> Modulus
+// (++) -> Inc
+// (--) -> Dec
+let a = 20;
+let b = 10;
+
+console.log(a + b); // a,b -> Operand; + -> operator | (+)
+console.log(20 - 10); // a,b -> Operand; + -> operator (-)
+console.log(2 * 6); // (*)
+console.log(a / b); // (/)
+// 3^5
+// 3*3*3*3*3 =>
+
+/**
+ * TODO
+ * (a+b)^2
+ * a**2 + 2*a*b + b**2
+ */
+console.log(3 ** 5); // 243
+console.log(a % b); // 0
+
+// inc && dec
+console.log(++a); // 21
+console.log(--b); // 9
+
+// ================================== //
+// Unary
+let c = 50;
+let d = "100";
+console.log(++c); // 51
+console.log(+c); // nigation
+
+console.log(typeof +d);
+
+// ================================== //
+// Binary Operator
+const num1 = 20;
+const num2 = 30;
+const result = num1 + num2; // binary operator ar example
+console.log(result);
