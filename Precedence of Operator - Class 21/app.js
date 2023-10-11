37879e8b86f903d8f76ae82e84327c279f73cdaf
devlopersabbir <devlopersabbir@gmail.com>
2023-10-11 19:01:25 +0600

    👨‍💻 Precedence of operator 📌

---
+++ Precedence of Operator - Class 21/app.js
@@ -0,0 +1,12 @@
+const a = 4;
+const b = 2;
+
+console.log(a + b * 3); // 4 + 2 = ? * 3 = 10
+
+console.log(12 / 2 - b); // 4
+
+const result = 4 * 2 + 40 / 2 - 1; // 4 * 2 = ? + 40 / 2 = ? - 2 = ?
+console.log(result);
+
+// expression
+console.log((2 + 5) * 3); // 21
