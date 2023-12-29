07fd81b43d3684d203ba84ad7a69a295b11926ee
devlopersabbir <devlopersabbir@gmail.com>
2023-12-29 23:46:12 +0600

    [feat ✔] inc | dec & postfix | prefix

---
+++ increment_dicrement - Class 23/app.js
@@ -0,0 +1,41 @@
+// let a = 5;
+// a += 1; // 6
+// a = a + 1; // 5 + 1
+// console.log(a);
+
+/**
+ * Increment
+ * (++)
+ */
+// const num = 0;
+// num += 1; // 1
+// num += 1; // 2
+// num += 1; // 3
+// num += 1; // 4
+
+// num++; // 1
+// num++; // 2
+// num++; // 3
+
+// console.log(num);
+/**
+ * Decrement
+ * (--)
+ */
+// let num = 5;
+// num--;
+// console.log(num);
+
+// postfix
+// num++; example
+let num = 5;
+let a = num++;
+console.log("result", a); // 5
+console.log(num); // 6
+// prefix
+// ++num; example
+let num1 = 5;
+let b = ++num1; // 6
+++num1; // 7
+console.log("result", b); // 6
+console.log(num1); // 7
