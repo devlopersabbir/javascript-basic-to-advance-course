cb4428beccb15233af5d2696e3c81877b75d9718
devlopersabbir <devlopersabbir@gmail.com>
2024-03-30 15:33:24 +0600

    [docs 🐱‍🏍] compare

--- comparison_operator_class_25/app.js
+++ comparison_operator_class_25/app.js
@@ -1,8 +1,23 @@
 /**
+ *  == ----  EE
+ * != ----  NE
+ * === ---  EV-ET
+ * !== ---  NEV-NET
  * >  ----  GT
  * <  ----  LT
  * >= ----  GE
  * <= ----  LE
- * == ----  EE
- * != ----  NE
  */
+
+// const num1 = 5;
+// const result = "5" !== 5; // boolean
+// // const result1 = "5" != 5; // boolean
+
+// console.log(result);
+// // console.log(result1);
+
+const result = 3 >= 7; // boolean
+console.log(result);
+
+// < -> ata obosooi boro hote hobe (true)
+// > -> ata obosooi chot hote hobe (true)
